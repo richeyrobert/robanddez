f9bf83ab7ce4b30368185d6a8481d876719d0946
Robert Richey <rob@rocketnext.com>
2013-07-31 19:34:31 -0600

    Adds foundation template, devise and authentication.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Robanddez::Application.routes.draw do
   authenticated :user do
     root :to => 'home#index'
   end
-  root :to => "home#index"
+  root :to => "static#index"
   devise_for :users
   resources :users
 end
