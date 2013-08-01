572350ad7c9048f2c1ee43078a44dedc6dea6ba2
Robert Richey <rob@rocketnext.com>
2013-07-31 18:06:50 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robanddez::Application.config.secret_token = '1ac1924c165639e73093782af27f178946891d9dc70bc23983c8e3383e5faba799252858d0294fd83235d4171c1dfbc647868875c08624044ca6c7346358cfb4'
